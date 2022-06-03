e11c302512c6a3e33c60901cd501b34e24c147d3
dorellanag92 <killertheworm@hotmail.com>
2022-06-03 12:09:26 -0400

    Solo generacion de datos web

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -13,9 +13,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "66ff9c16a0797c6854bf2dc9d5afb034",
-"/": "66ff9c16a0797c6854bf2dc9d5afb034",
-"main.dart.js": "59a0660a87e03b40f5b5be35746f4b43",
+"index.html": "b7c3505e4d5129aa063a9cbc6ee1f3c0",
+"/": "b7c3505e4d5129aa063a9cbc6ee1f3c0",
+"main.dart.js": "0916a311c99aa7c99e3eaee06c9e159e",
 "manifest.json": "e235c43e31efd6ada85d20ccda0f58bb",
 "version.json": "05f257e883517c55810c9e0427122e7d"
 };
